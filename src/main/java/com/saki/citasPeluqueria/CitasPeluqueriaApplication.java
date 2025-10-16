1d0e26efb2393cceecfc5386bdafc7f514ad4db6
husnain <hchaudhry@gestion400.com>
2025-10-16 23:01:15 +0200

    Creacion del war

--- src/main/java/com/saki/citasPeluqueria/CitasPeluqueriaApplication.java
+++ src/main/java/com/saki/citasPeluqueria/CitasPeluqueriaApplication.java
@@ -2,9 +2,16 @@ package com.saki.citasPeluqueria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CitasPeluqueriaApplication {
+public class CitasPeluqueriaApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CitasPeluqueriaApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CitasPeluqueriaApplication.class, args);
