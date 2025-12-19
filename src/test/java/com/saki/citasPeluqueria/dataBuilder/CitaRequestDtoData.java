6b46e6df51f5e2a4d1b561ba2571a9c2a895b5c1
husna4 <husnainmuhammad09@gmail.com>
2025-12-19 19:25:18 +0100

    Pruebas JUnit validacion campos DTO

---
+++ src/test/java/com/saki/citasPeluqueria/dataBuilder/CitaRequestDtoData.java
@@ -0,0 +1,48 @@
+package com.saki.citasPeluqueria.dataBuilder;
+
+import com.saki.citasPeluqueria.dto.CitaRequestDto;
+import com.saki.citasPeluqueria.dto.ClienteAnonimoDto;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+
+/**
+ * @author husnain
+ */
+
+@Getter
+public enum CitaRequestDtoData {
+
+    SIN_FECHA(CitaRequestDto.builder()
+            .hora(LocalTime.of(10, 0))
+            .clienteAnonimo(ClienteAnonimoDto.builder().nombre("Junit1").build())
+            .build()),
+
+    SIN_HORA(CitaRequestDto.builder()
+            .fecha(LocalDate.of(2021, 1, 1))
+            .clienteAnonimo(ClienteAnonimoDto.builder().nombre("Junit1").build())
+            .build()),
+
+    SIN_CLIENTE(CitaRequestDto.builder()
+            .fecha(LocalDate.of(2021, 1, 1))
+            .hora(LocalTime.of(10, 0))
+            .build()),
+
+    SIN_FECHA_HORA_CLIENTE_CORTES(CitaRequestDto.builder()
+            .build()),
+
+    VALIDA(CitaRequestDto.builder()
+            .fecha(LocalDate.of(2021,1,1))
+            .hora(LocalTime.of(11,0))
+            .idCliente(12344L)
+            .build())
+    ;
+
+    private final CitaRequestDto citaDto;
+
+    CitaRequestDtoData(CitaRequestDto citaDto) {
+        this.citaDto = citaDto;
+    }
+}
