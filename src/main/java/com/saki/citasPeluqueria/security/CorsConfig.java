3bcce877eb75e27401fcacedef10433a70a49907
husna4 <husnainmuhammad09@gmail.com>
2026-01-05 17:18:35 +0100

    Configuracion Cors

---
+++ src/main/java/com/saki/citasPeluqueria/security/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.saki.citasPeluqueria.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    private static final String[] ALLOWED_ORIGINS = {"http://localhost:5500",
+            "http://127.0.0.1:5500",
+            "http://localhost:5501",
+            "http://127.0.0.1:5501"};
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(ALLOWED_ORIGINS)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
